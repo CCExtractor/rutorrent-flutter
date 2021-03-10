cb7d32c8bcccfed2097acac394ba9b5d913b6dd1
Rajan Jaiswal <rajanjwlcode@gmail.com>
2021-03-11 05:27:42 +0530

    fix: UI bug in the AppBottomSheet bottom padding

--- lib/components/add_url_bottom_sheet.dart
+++ lib/components/add_url_bottom_sheet.dart
@@ -47,111 +47,107 @@ class _AddBottomSheetState extends State<AddBottomSheet> {
   @override
   Widget build(BuildContext context) {
     double wp = MediaQuery.of(context).size.width;
-    double bottomPadding = MediaQuery.of(context).viewInsets.bottom;
-    return Padding(
-      padding: EdgeInsets.only(bottom: bottomPadding),
-      child: Container(
-        height: 300,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: wp * 0.35,
-              ),
-              height: 5,
-              color: Theme.of(context).primaryColor,
+    return Container(
+      height: 300,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: wp * 0.35,
             ),
-            Text(
-              'Add link',
-              style: TextStyle(
-                  fontSize: 14,
-                  color: Provider.of<Mode>(context).isLightMode
-                      ? Colors.black54
-                      : Colors.white),
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: DataInput(
-                borderColor: Provider.of<Mode>(context).isLightMode
+            height: 5,
+            color: Theme.of(context).primaryColor,
+          ),
+          Text(
+            'Add link',
+            style: TextStyle(
+                fontSize: 14,
+                color: Provider.of<Mode>(context).isLightMode
+                    ? Colors.black54
+                    : Colors.white),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: DataInput(
+              borderColor: Provider.of<Mode>(context).isLightMode
+                  ? Theme.of(context).primaryColor
+                  : Colors.white,
+              textEditingController: urlTextController,
+              hintText: widget.dialogHint,
+              focus: urlFocus,
+              suffixIconButton: IconButton(
+                color: Provider.of<Mode>(context).isLightMode
                     ? Theme.of(context).primaryColor
                     : Colors.white,
-                textEditingController: urlTextController,
-                hintText: widget.dialogHint,
-                focus: urlFocus,
-                suffixIconButton: IconButton(
-                  color: Provider.of<Mode>(context).isLightMode
-                      ? Theme.of(context).primaryColor
-                      : Colors.white,
-                  onPressed: () async {
-                    ClipboardData data = await Clipboard.getData('text/plain');
-                    if (data != null)
-                      urlTextController.text = data.text.toString();
-                    if (urlFocus.hasFocus) urlFocus.unfocus();
-                  },
-                  icon: Icon(Icons.content_paste),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 36),
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                      side: BorderSide(color: Theme.of(context).primaryColor),
-                    ),
-                    side: BorderSide(color: Theme.of(context).primaryColor),
-                    primary: Theme.of(context).primaryColor),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 28, vertical: 16),
-                  child: Text(
-                    'Start Download',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-                onPressed: () {
-                  widget.apiRequest(urlTextController.text);
-                  Navigator.pop(context);
+                onPressed: () async {
+                  ClipboardData data = await Clipboard.getData('text/plain');
+                  if (data != null)
+                    urlTextController.text = data.text.toString();
+                  if (urlFocus.hasFocus) urlFocus.unfocus();
                 },
+                icon: Icon(Icons.content_paste),
               ),
             ),
-            (widget.dialogHint == "Enter Rss Url")
-                ? Container()
-                : Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16, horizontal: 36),
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                            side: BorderSide(
-                                color: Theme.of(context).primaryColor),
-                          ),
-                          side:
-                              BorderSide(color: Theme.of(context).primaryColor),
-                          primary: Theme.of(context).primaryColor),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 28, vertical: 16),
-                        child: Text(
-                          'Browse Torrent File',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      ),
-                      onPressed: () {
-                        pickTorrentFile();
-                        Navigator.pop(context);
-                      },
-                    ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 36),
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Theme.of(context).primaryColor),
                   ),
-          ],
-        ),
+                  side: BorderSide(color: Theme.of(context).primaryColor),
+                  primary: Theme.of(context).primaryColor),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 28, vertical: 16),
+                child: Text(
+                  'Start Download',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              onPressed: () {
+                widget.apiRequest(urlTextController.text);
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          (widget.dialogHint == "Enter Rss Url")
+              ? Container()
+              : Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16, horizontal: 36),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: BorderSide(
+                              color: Theme.of(context).primaryColor),
+                        ),
+                        side:
+                            BorderSide(color: Theme.of(context).primaryColor),
+                        primary: Theme.of(context).primaryColor),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 28, vertical: 16),
+                      child: Text(
+                        'Browse Torrent File',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                    onPressed: () {
+                      pickTorrentFile();
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+        ],
       ),
     );
   }
