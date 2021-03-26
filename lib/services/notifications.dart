d5bff0ee285ea79e44f13adba1c0e0720b8bc6ca
Abdul Malik <160418733095@mjcollege.ac.in>
2021-03-26 12:45:01 +0530

    refactor : Setting Notification Channel ID, Name and Description

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rutorrentflutter/models/general_features.dart';
+import 'package:rutorrentflutter/utilities/constants.dart';
 
 /// Notifications class using flutter local notifications to generate Alert
 
@@ -20,20 +24,32 @@ class Notifications {
         onSelectNotification: (String payload) {
       return;
     });
-
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        'your channel id', 'your channel name', 'your channel description',
-        importance: Importance.max, priority: Priority.high);
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    platformChannelSpecifics = new NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
   }
 
   /// Public Method to generate Notification
-  generate(String header, String body) async {
+  generate(String header, String body, NotificationChannelID id) async {
+
+    //Setting Notification Channel ID, Name and Description
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        id.index.toString(), 
+        notificationInfo[id.index][0],
+        notificationInfo[id.index][0],
+        importance: Importance.max, 
+        priority: Priority.high
+    );
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    platformChannelSpecifics = new NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics
+    );
+
+    //Notification ID
+    Random random = new Random();
+    int notificationId = random.nextInt(100);
+
+    //Show notification to user
     await flutterLocalNotificationsPlugin.show(
-      0,
+      notificationId, // Index of enum element
       header,
       body,
       platformChannelSpecifics,
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -19,6 +19,19 @@ const Color kRedErrorDT = Color(0xFFEF5350);
 const Color kGreenActiveDT = Color(0xFF1ED760);
 const Color kBackgroundDT = Color(0xFF121212);
 
+///Notification Channel Names and Descriptions
+///
+///[Key] : Enum index of corresponding notification channel ID
+///
+///[Value] : List 
+///        First element being Notification Channel Name, 
+///        Second element being Notification Channel Description
+Map<int,List<String>> notificationInfo = {
+  0 : ["Torrent Added Channel","This Channel is for notifying when a new Torrent is added"],
+  1 : ["Download Completed Channel","This Channel is for notifying when a download is completed"],
+  2 : ["Low Disk Space Channel","This Channel is for notifying when there is Low Disk Space"],
+};
+
 /// APP RELEASE INFO
 const String BUILD_NUMBER = '2';
 const String RELEASE_DATE = '28.02.20';
