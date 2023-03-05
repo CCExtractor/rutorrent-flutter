bb8abec4c36c4d6258536358f301afd8e8328139
Rushikesh Patade <pataderushikesh@gmail.com>
2023-03-05 18:09:06 +0530

    "flutter analyze" running

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
 import 'package:path_provider/path_provider.dart';
@@ -15,8 +14,6 @@ import 'package:rutorrentflutter/services/state_services/user_preferences_servic
 import 'package:rutorrentflutter/theme/app_state_notifier.dart';
 import 'package:rutorrentflutter/theme/app_theme.dart';
 import 'package:rutorrentflutter/ui/widgets/smart_widgets/bottom_sheets/bottom_sheet_setup.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -41,7 +38,6 @@ void main() async {
     // await Firebase.initializeApp(
     //   options: DefaultFirebaseOptions.currentPlatform,
     // );
-
 
     // Make sure to comment out this line in development to see Errors
     // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
