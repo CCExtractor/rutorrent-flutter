e740334b864f07612d312536bf9f5980e78be568
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2023-05-09 15:05:06 +0530

    Update bottom_sheet_setup.dart
    fix: Removed deprecated FlatButtons and fix some issues

--- lib/ui/widgets/smart_widgets/bottom_sheets/bottom_sheet_setup.dart
+++ lib/ui/widgets/smart_widgets/bottom_sheets/bottom_sheet_setup.dart
@@ -85,7 +85,7 @@ class _FloatingBoxBottomSheet extends StatelessWidget {
                           fontWeight: FontWeight.bold),
                     ),
                   ),
-                  FlatButton(
+                  ElevatedButton(
                     onPressed: () =>
                         model.response(completer, textFieldController1.text),
                     child: Text(
@@ -93,7 +93,9 @@ class _FloatingBoxBottomSheet extends StatelessWidget {
                       style: TextStyle(
                           color: Colors.white, fontWeight: FontWeight.bold),
                     ),
-                    color: Theme.of(context).primaryColor,
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Theme.of(context).primaryColor
+                    ),
                   )
                 ],
               )
